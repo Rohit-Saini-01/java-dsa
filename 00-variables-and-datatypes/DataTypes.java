ed018c63c428a7d21b8f754c9b1e74e392999008
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-22 12:48:09 +0530

    data types

---
+++ 00-variables-and-datatypes/DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String args[]) {
+        int var_1 = 100; // 4 bytes
+        char var_2 = 'a'; // 1 byte
+        boolean var_3 = true; // 1 byte
+        float var_4 = 1.33f; // 4 byte
+        byte var_5 = 8; // 1 byte
+        short var_6 = 240; // 2 byte
+        long var_7 = 123456789; // 8 bytes
+        double var_8 = 1.333; // 4 bytes
+
+        System.out.println(var_1);
+        System.out.println(var_2);
+        System.out.println(var_3);
+        System.out.println(var_4);
+        System.out.println(var_5);
+        System.out.println(var_6);
+        System.out.println(var_7);
+        System.out.println(var_8);
+    }
+}
