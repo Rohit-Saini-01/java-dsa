7bf305c8710445e6c430258739d27bca9ab15eed
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-22 11:32:38 +0530

    variables

---
+++ 00-variables-and-datatypes/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String args[]) {
+        int var_1 = 10;
+        int var_2 = 20;
+        String name = "Rohit Saini";
+
+        System.out.println(var_1);
+        System.out.println(var_2);
+        System.out.println(name);
+    }
+}
