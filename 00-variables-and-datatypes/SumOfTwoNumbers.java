54e86d6b7cf5b1e49ff9be3b4ee2f6eeb991ecde
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-22 12:58:20 +0530

    sum of two numbers

---
+++ 00-variables-and-datatypes/SumOfTwoNumbers.java
@@ -0,0 +1,8 @@
+public class SumOfTwoNumbers {
+    public static void main(String args[]) {
+        int num_1 = 10;
+        int num_2 = 20;
+        int sum = num_1 + num_2;
+        System.out.println(sum);
+    }
+}
