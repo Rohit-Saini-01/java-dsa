b11bf90a3ffcf85b542cd5a58675fcbae87d39e8
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-25 11:45:04 +0530

    sum of two integers

---
+++ _00_VariablesAndDataTypes/_05_SumOfTwoIntegers.java
@@ -0,0 +1,9 @@
+public class _05_SumOfTwoIntegers {
+    public static void main(String[] args) {
+        int num_1 = 10;
+        int num_2 = 5;
+        int sum = num_1 + num_2;
+
+        System.out.println(sum);
+    }
+}
