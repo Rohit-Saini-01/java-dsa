d4808266985b3fa3332bbd098d4dd58fb1730f8e
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-25 17:38:42 +0530

    typing casting

---
+++ _00_VariablesAndDataTypes/_11_TypeCasting.java
@@ -0,0 +1,8 @@
+public class _11_TypeCasting {
+    public static void main(String[] args) {
+        float var_1 = 3.1415f;
+        int var_2 = (int) var_1;
+
+        System.out.println(var_2);
+    }
+}
