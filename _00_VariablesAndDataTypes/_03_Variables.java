09a4f81595884b6dd0109e3704d08e7b3fe84ade
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-25 10:48:55 +0530

    variable declaration and initialisation

---
+++ _00_VariablesAndDataTypes/_03_Variables.java
@@ -0,0 +1,7 @@
+public class _03_Variables {
+    public static void main(String[] args) {
+        int num = 10;
+        String name = "Rohit Saini";
+        System.out.println(num +" "+ name);
+    }
+}
