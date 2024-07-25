b1f2b713f8b85502ff66650c5f9a5bbd58e0cb0a
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-25 11:50:54 +0530

    comments in java

---
+++ _00_VariablesAndDataTypes/_06_CommentsInJava.java
@@ -0,0 +1,10 @@
+public class _06_CommentsInJava {
+    public static void main(String[] args) {
+        // I am a single-line comment. I will not be compiled.
+
+        /*
+         * I am a multi-line comment,
+         * I will also not be compiled.
+         */
+    }
+}
