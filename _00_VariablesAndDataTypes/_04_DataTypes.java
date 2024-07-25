1a435e2c0e9673f9ea9244107e69ca9587b6b26e
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-25 11:40:36 +0530

    primitive datatypes in java

---
+++ _00_VariablesAndDataTypes/_04_DataTypes.java
@@ -0,0 +1,15 @@
+public class _04_DataTypes {
+    public static void main(String[] args) {
+        byte var_1 = 11;
+        char var_2 = 'A';
+        short var_3 = 120;
+        int var_4 = 10;
+        float var_5 = 3.14f;
+        double var_6 = 3.1415;
+        long var_7 = 1234567;
+        boolean var_8 = true;
+
+        System.out.print(var_1 + "\n" + var_2 + "\n" + var_3 + "\n" + var_4 + "\n" + var_5 + "\n" + var_6 + "\n" + var_7
+                + "\n" + var_8 + "\n");
+    }
+}
