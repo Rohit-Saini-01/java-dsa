0f5c90e48c7e445e8eeb8d63b598f4aaf95e079b
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-26 14:47:47 +0530

    logical operators

---
+++ _01_Operators/_03_LogicalOperators.java
@@ -0,0 +1,14 @@
+package _01_Operators;
+
+public class _03_LogicalOperators {
+    public static void main(String[] args) {
+        boolean var_1 = true;
+        boolean var_2 = false;
+
+        System.out.println(var_1&&var_2);
+        System.out.println(var_1||var_2);
+        System.out.println(!var_2);
+        System.out.println(!var_1);
+
+    }
+}
