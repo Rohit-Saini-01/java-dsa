25666d9b96e1c366e074cbf93d38a4b8810b9783
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-26 14:45:06 +0530

    relational operators

---
+++ _01_Operators/_02_RelationalOperator.java
@@ -0,0 +1,15 @@
+package _01_Operators;
+
+public class _02_RelationalOperator {
+    public static void main(String[] args) {
+        int var_1 = 10;
+        int var_2 = 5;
+
+        System.out.println(var_1 > var_2);
+        System.out.println(var_1 < var_2);
+        System.out.println(var_1 >= var_2);
+        System.out.println(var_1 <= var_2);
+        System.out.println(var_1 == var_2);
+        System.out.println(var_1 != var_2);
+    }
+}
