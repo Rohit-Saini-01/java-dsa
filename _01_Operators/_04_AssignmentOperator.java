ede0c8ec1ab8eb23d5c5669ab008018c4e83ab02
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-26 16:28:13 +0530

    assignment operator

---
+++ _01_Operators/_04_AssignmentOperator.java
@@ -0,0 +1,19 @@
+package _01_Operators;
+
+public class _04_AssignmentOperator {
+    public static void main(String[] args) {
+        int var_1 = 10;
+
+        var_1 += 5;
+        System.out.println(var_1);
+
+        var_1 -= 5;
+        System.out.println(var_1);
+
+        var_1 *= 5;
+        System.out.println(var_1);
+
+        var_1 /= 5;
+        System.out.println(var_1);
+    }
+}
