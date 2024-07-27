c18b67623e6a5841a58df05d40853465029f2b7e
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-27 14:24:49 +0530

    while loop

---
+++ _03_Loops/_00_WhileLoop.java
@@ -0,0 +1,14 @@
+package _03_Loops;
+
+public class _00_WhileLoop {
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        while (counter < 10) {
+            System.out.println("Inside while loop");
+            counter++;
+        }
+
+    }
+}
