72ccef03daafac56488524aa64f4011e87cf40c4
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-27 14:26:40 +0530

    print numbers 1 to 10

---
+++ _03_Loops/_01_PrintNumbers1To10.java
@@ -0,0 +1,13 @@
+package _03_Loops;
+
+public class _01_PrintNumbers1To10 {
+    public static void main(String[] args) {
+
+        int counter = 1;
+
+        while (counter <= 10) {
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
