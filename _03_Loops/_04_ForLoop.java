c688e5a19a51a167600d2a00f0af32817929a4ad
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-28 12:22:36 +0530

    for loop

---
+++ _03_Loops/_04_ForLoop.java
@@ -0,0 +1,10 @@
+package _03_Loops;
+
+public class _04_ForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(" Inside For Loop");
+        }
+    }
+}
