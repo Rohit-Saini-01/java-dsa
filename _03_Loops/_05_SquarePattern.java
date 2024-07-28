de05f52b5539ad18b8b2b348512235a8bc765b12
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-28 12:27:13 +0530

    square pattern

---
+++ _03_Loops/_05_SquarePattern.java
@@ -0,0 +1,10 @@
+package _03_Loops;
+
+public class _05_SquarePattern {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("****");
+        }
+    }
+}
