f849341dfce9c35b2b50ca6a920e42619ce47280
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-28 12:53:21 +0530

    break statement

---
+++ _03_Loops/_08_BreakStatement.java
@@ -0,0 +1,13 @@
+package _03_Loops;
+
+public class _08_BreakStatement {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                break;
+            }
+            System.out.println(i);
+        }
+        System.out.println("Outside of for loop");
+    }
+}
