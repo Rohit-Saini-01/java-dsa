3fb108bd57181aeb62a5a2525e75a3d0ee00ad6a
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-28 12:47:40 +0530

    do-while loop

---
+++ _03_Loops/_07_DoWhileLoop.java
@@ -0,0 +1,11 @@
+package _03_Loops;
+
+public class _07_DoWhileLoop {
+    public static void main(String[] args) {
+        int counter = 1;
+        do {
+            System.out.println("Inside do-while loop");
+            counter++;
+        } while (counter <= 10);
+    }
+}
