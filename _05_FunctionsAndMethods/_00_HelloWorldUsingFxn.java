46541e38620cb3490dce99998b143f132b40ef1c
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-28 14:55:16 +0530

    hello world using fxn

---
+++ _05_FunctionsAndMethods/_00_HelloWorldUsingFxn.java
@@ -0,0 +1,12 @@
+package _05_FunctionsAndMethods;
+
+public class _00_HelloWorldUsingFxn {
+    public static void printHelloWorld() {
+        System.out.println(" Function says Hello!");
+
+    }
+
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}
